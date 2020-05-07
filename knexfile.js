759a80340358a04ee6858de53c84559d3ad08bbd
marcelosdias <55857919+marcelosdias@users.noreply.github.com>
2020-05-07 01:31:38 -0300

    Fix heroku

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,11 @@ module.exports = {
   development: {
     client: process.env.DB_CLIENT,
     connection: {
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT
     },
     migrations: {
       directory: `${__dirname}/src/database/migrations`
