6e5275de7c8c5814cd44a53617bebe68d350c543
marcelosdias <55857919+marcelosdias@users.noreply.github.com>
2020-05-07 01:02:31 -0300

    Add arquivos heroku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const apiRoutes = require('./routes')
+const cors = require('cors')
 const app = express()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(apiRoutes)
 
